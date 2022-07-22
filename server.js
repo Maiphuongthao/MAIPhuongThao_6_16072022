6c84b3656cf894f129ed7e4ba9fb89883e581e5a
Thao <thao.maiphuong@outlook.fr>
2022-07-22 22:02:08 +0200

    add router inder in server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 require("./app/config/db.config");
+const router = require('./app/routes/index');
+
 
 
 const app = express();
