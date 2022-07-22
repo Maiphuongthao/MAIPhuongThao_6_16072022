5d7b79069f1a298ebe1bf6051a246c5a7841d274
Thao <thao.maiphuong@outlook.fr>
2022-07-22 21:59:00 +0200

    add user routes path in index

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,16 +1,17 @@
 const bcrypt = require("bcrypt");
 
 //import cryptojs for encrypt email
-const cryptojs = require('crypto-js');
+const cryptojs = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 require("dotenv").config();
 
 exports.signup = (req, res, next) => {
-
-//Encrypt email before sending it to database
-const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.CLE_SECRETE).toString();
+  //Encrypt email before sending it to database
+  const emailCryptoJs = cryptojs
+    .HmacSHA256(req.body.email, process.env.CLE_SECRETE)
+    .toString();
   bcrypt
     //call function hash by bcrypt to password with a salt 10 times to make it safer
     //create an user and save it in database, send message if it's created and error if not
@@ -49,7 +50,7 @@ exports.login = (req, res, next) => {
               //userId entant playload
               { userId: user._id },
               //random token dispo pendant 24h
-            process.env.RANDOM_TOKEN_SECRET,
+              process.env.RANDOM_TOKEN_SECRET,
               { expiresIn: "24h" }
             ),
           });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,1 +1,9 @@
-require('../routes/user');
+const express = require('express');
+const router = express.Router();
+const userRoutes = require('./user');
+
+
+//path for user routes
+app.use('/api/auth', userRoutes);
+
+module.exports = router;
