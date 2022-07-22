f77ffc38b877c0363d543d6f1d5084064c5eecd1
Thao <thao.maiphuong@outlook.fr>
2022-07-22 21:50:44 +0200

    require cryotpjs and add it to signup function to encrypt the email before add it to database, secret key and random password are put in env

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,52 +1,60 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+
+//import cryptojs for encrypt email
+const cryptojs = require('crypto-js');
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
-require('dotenv').config();
+require("dotenv").config();
 
 exports.signup = (req, res, next) => {
-  bcrypt
 
-  //call function hash by bcrypt to password with a salt 10 times to make it safer
-  //create an user and save it in database, send message if it's created and error if not
+//Encrypt email before sending it to database
+const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.CLE_SECRETE).toString();
+  bcrypt
+    //call function hash by bcrypt to password with a salt 10 times to make it safer
+    //create an user and save it in database, send message if it's created and error if not
     .hash(req.body.password, 10)
     .then((hash) => {
       const user = new User({
-        email: req.body.email,
-        password: hash
+        email: emailCryptoJs,
+        password: hash,
       });
       user
         .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .then(() => res.status(201).json({ message: "User created !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        //chiffrer un nouveau token
-                        token: jwt.sign(
-                            //userId entant playload
-                            { userId: user._id },
-                            //random token dispo pendant 24h
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "User not found !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ error: "Your password is incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            //chiffrer un nouveau token
+            token: jwt.sign(
+              //userId entant playload
+              { userId: user._id },
+              //random token dispo pendant 24h
+            process.env.RANDOM_TOKEN_SECRET,
+              { expiresIn: "24h" }
+            ),
+          });
         })
-        .catch(error => res.status(500).json({ error }));
- };
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
