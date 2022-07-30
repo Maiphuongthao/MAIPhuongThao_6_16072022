b055a148a7671ea50aa718e3d1f5d2105f988624
Thao <thao.maiphuong@outlook.fr>
2022-07-30 22:19:27 +0200

    add express-slow-down to server

--- server.js
+++ server.js
@@ -36,6 +36,20 @@ app.use("/api", router);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 
+//Add slowdown speed limiter
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 100, // allow 100 requests per 15 minutes, then...
+  delayMs: 500 // begin adding 500ms of delay per request above 100:
+  // request # 101 is delayed by  500ms
+  // request # 102 is delayed by 1000ms
+  // request # 103 is delayed by 1500ms
+  // etc.
+});
+//  apply to all requests
+app.use(speedLimiter);
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
