b80ee4086b2a2a63cc8489a2cf28ab48d0bed3b5
Thao <thao.maiphuong@outlook.fr>
2022-08-05 11:43:03 +0200

    prettier

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controllers/user");
-const ratelimiter = require("../middleware/rate-limit")
-const auth = require("../middleware/auth")
+const ratelimiter = require("../middleware/rate-limit");
+const auth = require("../middleware/auth");
 
 router.post("/signup", userCtrl.signup);
-router.post("/login",ratelimiter, userCtrl.login);
-router.get('/', auth, userCtrl.readUser);
-router.get('/export', auth, userCtrl.exportData);
-router.put('/', auth, userCtrl.updateUser);
-router.delete('/', auth, userCtrl.deleteUser);
+router.post("/login", ratelimiter, userCtrl.login);
+router.get("/", auth, userCtrl.readUser);
+router.get("/export", auth, userCtrl.exportData);
+router.put("/", auth, userCtrl.updateUser);
+router.delete("/", auth, userCtrl.deleteUser);
 
 module.exports = router;
